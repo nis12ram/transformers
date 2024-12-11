e5a79dc6afe96d197255aa6aa43dc4da9d285109
nis12ram <nisramgcol@gmail.com>
2024-12-11 17:03:10 +0530

    _

--- src/transformers/models/bitnet.py
+++ src/transformers/models/bitnet.py
@@ -27,24 +27,63 @@ class SignSTE(nn.Module):
     def forward(self, input):
         return SignSTEFunc.apply(input)
 
+## for one bit paper
+# class BitLinear(nn.Module):
+#     def __init__(self, in_features, out_features, groups=1, bias=False, device=None, dtype=None):
+#         factory_kwargs = {'device': device, 'dtype': dtype}
+#         super().__init__()
+#         self.in_features = in_features
+#         self.out_features = out_features
+#         self.groups = groups            # now omitted!
+#         self.weight = nn.Parameter(torch.empty((out_features, in_features), **factory_kwargs))
+#         self.weight_scale = nn.Parameter(torch.empty(out_features, **factory_kwargs))
+#         self.sign = SignSTE()
+#         self.input_factor = nn.Parameter(torch.empty(in_features, **factory_kwargs))
+        
+#         if bias:
+#             self.bias = nn.Parameter(torch.empty(out_features, **factory_kwargs))
+#         else:
+#             self.register_parameter('bias', None)
+#         self.layernorm = nn.LayerNorm(out_features, elementwise_affine=False)
+#         self.reset_parameters()
 
+#     def reset_parameters(self):
+#         init.constant_(self.weight_scale, 1.0)
+#         init.constant_(self.input_factor, 1.0)
+#         if self.bias is not None:
+#             fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+#             bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+#             init.uniform_(self.bias, -bound, bound)
+            
+#     def forward(self, input):
+#         input = input * self.input_factor.view(1, self.in_features)
+#         weight = self.sign(self.weight)
+#         output = F.linear(input, weight)
+#         output *= self.weight_scale.view(1, self.out_features)
+        
+#         output = self.layernorm(output)
+#         if self.bias is not None:
+#             output += self.bias
+        
+#         return output
+ 
+## for simple svd 
 class BitLinear(nn.Module):
-    def __init__(self, in_features, out_features, groups=1, bias=False, device=None, dtype=None):
+    def __init__(self, in_features, out_features, groups=1, rank = 50, bias=False, device=None, dtype=None):
         factory_kwargs = {'device': device, 'dtype': dtype}
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.groups = groups            # now omitted!
-        self.weight = nn.Parameter(torch.empty((out_features, in_features), **factory_kwargs))
-        self.weight_scale = nn.Parameter(torch.empty(out_features, **factory_kwargs))
-        self.sign = SignSTE()
-        self.input_factor = nn.Parameter(torch.empty(in_features, **factory_kwargs))
+        self.rank = rank
+        # self.W = nn.Parameter(torch.empty(out_features, **factory_kwargs))
+        # self.H = nn.Parameter(torch.empty(in_features, **factory_kwargs))
+        self.W = nn.Parameter(torch.empty((out_features, rank), **factory_kwargs))
+        self.H = nn.Parameter(torch.empty((rank, in_features), **factory_kwargs))
         
         if bias:
             self.bias = nn.Parameter(torch.empty(out_features, **factory_kwargs))
         else:
             self.register_parameter('bias', None)
-        self.layernorm = nn.LayerNorm(out_features, elementwise_affine=False)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -56,17 +95,17 @@ class BitLinear(nn.Module):
             init.uniform_(self.bias, -bound, bound)
             
     def forward(self, input):
-        input = input * self.input_factor.view(1, self.in_features)
-        weight = self.sign(self.weight)
-        output = F.linear(input, weight)
-        output *= self.weight_scale.view(1, self.out_features)
+        # input = input * self.input_factor.view(1, self.in_features)
+        # weight = self.sign(self.weight)
+        # output = F.linear(input, weight)
+        # output *= self.weight_scale.view(1, self.out_features)
         
-        output = self.layernorm(output)
+        # output = self.layernorm(output)
+        output = F.linear(input, torch.matmul(self.W, self.H))
         if self.bias is not None:
             output += self.bias
         
-        return output
-    
+        return output   
     
 class BitLinearInf(nn.Module):
     def __init__(self, in_features, out_features, groups=1, bias=False, device=None, dtype=None):
